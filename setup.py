f5540ef46b5d372c3cebbdf663753677a7e0aebc
817772616 <77785344+817772616@users.noreply.github.com>
2021-07-04 10:32:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='immofind',
+    version='',
+    packages=['bs4', 'requests', 'selenium'],
+    url='',
+    license='',
+    author='your mom',
+    author_email='',
+    description=''
+)
